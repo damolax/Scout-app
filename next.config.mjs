92fae275b8de00c9680065c4003e1b4622571d49
damolax <oyekunleolalekan3168@gmail.com>
2026-07-07 20:27:11 +0100

    Build v8.8 simple email flow

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {},
-  outputFileTracingIncludes: {
-    '/api/classic': ['./legacy/scout-classic.html']
-  }
-};
-
-export default nextConfig;
